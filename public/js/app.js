18afa3819dcc20e02ca79a12ba585d3afc487fa5
rishidayani <rishidayani@gmail.com>
2023-02-16 10:35:38 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,26 @@
+console.log(`Client side javascript file is loaded!`)
+
+const weatherForm = document.querySelector(`form`)
+const search = document.querySelector(`input`)
+const mOne = document.querySelector(`#m1`)
+const mTwo = document.querySelector(`#m2`)
+
+weatherForm.addEventListener(`submit` , (e)=> {
+    e.preventDefault()
+
+    const location = search.value
+
+    mOne.textContent = `Loading....`
+    mTwo.textContent = ``
+
+    fetch( `http://localhost:4200/weather?address=${location}`).then((response) => {
+    response.json().then((data)=> {
+        if(data.error) {
+            mOne.textContent = data.error
+        } else {
+            mOne.textContent = data.location
+            mTwo.textContent = data.forecast
+        }
+    })
+})
+})
